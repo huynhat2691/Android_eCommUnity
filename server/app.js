f5ccfaaea8f67c9f5fbe29f511915b146adecc7c
HnB269 <nhathuy2691@gmail.com>
2025-01-02 19:25:57 +0700

    add

--- server/app.js
+++ server/app.js
@@ -24,6 +24,7 @@ app.use(
       "http://192.168.226.1:5173",
       "http://192.168.1.4:5000",
       "https://ecommunity-frontend.loca.lt",
+      "https://android-e-comm-unity-client.vercel.app/"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Thêm OPTIONS

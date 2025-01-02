b9dbb0513f72c759b5db8166f4f21ac1ae16ef31
HnB269 <nhathuy2691@gmail.com>
2025-01-02 19:23:45 +0700

    add

--- client/src/server.js
+++ client/src/server.js
@@ -2,10 +2,10 @@
 
 // export const backend_url = "http://localhost:5000/";
 
-export const server = "http://192.168.1.4:5000/api/v2";
+// export const server = "http://192.168.1.4:5000/api/v2";
 
-export const backend_url = "http://192.168.1.4:5000/";
+// export const backend_url = "http://192.168.1.4:5000/";
 
-// export const server = "https://ecommunity-backend.loca.lt/api/v2";
+export const server = "https://android-e-comm-unity-server.vercel.app/api/v2";
 
-// export const backend_url = "https://ecommunity-backend.loca.lt/";
+export const backend_url = "https://android-e-comm-unity-server.vercel.app/";
